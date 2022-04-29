548a961eff36bd6b73ecd52266e86a35f8ebff11
realDylandzn <depositodedylanmartinez@gmail.com>
2022-04-28 22:19:02 -0400

    Modifiqué el código para cumplir con el reto del curso, donde deberíamos añadir una sección de descuentos con cupones.

--- Taller_N2_PorcentajesYDescuentos/descuentos.js
+++ Taller_N2_PorcentajesYDescuentos/descuentos.js
@@ -1,15 +1,36 @@
-function descuento(precio, descuento){
+function descuentoF(precio, descuento){
     return (precio * (100 - descuento)) / 100;
 };
-
+const cupones = [{
+    name: "DSC15",
+    discount: 15,
+},{
+    name: "DSC25",
+    discount: 25,
+},{
+    name: "DSC30",
+    discount: 30,
+}];
 
 function calculoD(){
     const inputP = document.getElementById("InputPrecio");
     const valueP = inputP.value;
-    const inputD = document.getElementById("InputDescuento");
-    const valueD = inputD.value;
+    const inputC = document.getElementById("inputCupon");
+    const valueC = inputC.value;
 
-    const calculo = descuento(valueP, valueD);
-    const resultado = document.getElementById("resultado");
-    resultado.innerHTML = "El precio final es de: $" + calculo;
+    const cuponEsValido = function (cupon){
+        return cupon.name === valueC;
+    };
+    const userCupon = cupones.find(cuponEsValido);
+
+    if (!userCupon){
+        alert("El cupón "+ valueC + " es inválido.");
+    } else {
+        const descuento = userCupon.discount;
+        const precioConDescuento = descuentoF(valueP, descuento);
+        
+
+        const resultP = document.getElementById("resultado");
+        resultP.innerText = "El precio con descuento es $" + precioConDescuento + ".";
+    };
 };
